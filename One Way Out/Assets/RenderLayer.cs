853142b6f6a8dc198c579d206043333b77543dd7
Lotte <lotzzzzz2@gmail.com>
2021-06-18 16:55:44 +0200

    Added: Particle systems

---
+++ One Way Out/Assets/RenderLayer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderLayer : MonoBehaviour
+{
+   void Start ()
+     {
+             //Change Foreground to the layer you want it to display on 
+             //You could prob. make a public variable for this
+         //    GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingLayerName = "Foreground";
+     }
+}
